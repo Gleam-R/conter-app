f30dceb9f697074b0af021c38d7a9b93cc833d4b
Rizky <m.rizky29072004@gmail.com>
2025-11-13 09:20:40 +0700

    fix Error Strict Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [running, setIsRunning] = useState(false);
 
   useEffect(() => {
-    let interval;
+    let interval: NodeJS.Timeout;
     if (running) {
       interval = setInterval(() => {
         setSeconds((prev) => prev + 1);
